9c0051810aef94042cd55f0768bb9e76b2d6f60d
Caio Ayres <caio.ayres@encora.com>
2023-11-04 13:18:44 -0300

    Basic middleware implemented

--- client/client.go
+++ client/client.go
@@ -11,10 +11,28 @@ import (
 	pb "tcc/proto"
 )
 
+func UselessMiddleware(ctx context.Context, method string, req, reply any, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	ctxVal := ctx.Value("message")
+	if ctxVal == nil {
+		log.Println("Client middleware context value is nil")
+		ctxVal = 1
+	}
+	msg := req.(*pb.CountRequest)
+	log.Printf("Client middleware received: %v, with context value: %v", msg.GetMessage(), ctxVal)
+	ctxVal = ctxVal.(int) + 1
+	ctx = context.WithValue(ctx, "message", ctxVal)
+	msg.Message = "ClientMiddleware"
+	return invoker(ctx, method, msg, reply, cc, opts...)
+}
+
 func SendMessage(message string) {
 	flag.Parse()
 	// Set up a connection to the server.
-	conn, err := grpc.Dial("localhost:30301", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial(
+		"localhost:30301", 
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithUnaryInterceptor(UselessMiddleware),
+	)
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
@@ -23,6 +41,7 @@ func SendMessage(message string) {
 
 	// Contact the server and print out its response.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx = context.WithValue(ctx, "message", 1)
 	defer cancel()
 	r, err := c.Count(ctx, &pb.CountRequest{Message: message})
 	if err != nil {
--- server/server.go
+++ server/server.go
@@ -15,9 +15,27 @@ type server struct {
 }
 
 func (s *server) Count(ctx context.Context, in *pb.CountRequest) (*pb.CountReply, error) {
-	log.Printf("Received: %v", in.GetMessage())
+	ctxVal := ctx.Value("message")
+	if ctxVal == nil {
+		log.Println("Handle server context value is nil")
+	}
+	log.Printf("Server handler received: %v, with context value: %v", in.GetMessage(), ctxVal)
 	resp := pb.CountReply{MessageSize: int32(len(in.GetMessage()))}
 	return &resp, nil
+}
+
+func UselessMiddleware(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
+	ctxVal := ctx.Value("message")
+	if ctxVal == nil {
+		log.Println("Middleware server context value is nil")
+		ctxVal = 1
+	}
+	ctx = context.WithValue(ctx, "message", ctxVal)
+	msg := req.(*pb.CountRequest)
+	log.Printf("Server middleware received: %v, with context value: %v", msg.GetMessage(), ctxVal)
+	ctxVal = ctxVal.(int) + 1
+	msg.Message = "ServerMiddleware"
+	return handler(ctx, msg)
 }
 
 func StartServer() {
@@ -26,7 +44,9 @@ func StartServer() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
+	s := grpc.NewServer(
+		grpc.UnaryInterceptor(UselessMiddleware),
+	)
 	pb.RegisterCounterServer(s, &server{})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
